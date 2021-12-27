293c990967b59d95e1d374bd4b83f2372a62fd93
H.Zolf <digimat2017@gmail.com>
2021-12-27 19:21:55 +0100

    functions exercises with REZA

---
+++ functions.py
@@ -0,0 +1,9 @@
+def multiplytwonumbers(a,b):
+    return (a * b)
+    
+multiplytwonumbers(2 , 5)
+
+def multiplypower(a , b , c):
+    print(multiplytwonumbers(a**b, c))
+
+multiplypower(3 , 4 , 7)
